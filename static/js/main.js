5e8df2aa9fa29537923689ce0f67dc223a2af2e4
Фаниль Азмагулов <fanilazmagulov@Mac-mini-Fanil.local>
2020-10-27 14:34:27 +0500

    Поправил адаптивность

--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
     removalDelay: 300,
     mainClass: 'mfp-width-zoom'
   });
+
   
 
   $('#fullpage').fullpage({
@@ -25,73 +26,142 @@ $(document).ready(function(){
     anchors: ['advertising', 'funnel', 'custom', 'testing', 'conversion', 'selling', 'install', 'price', 'order', 'cases'],
     parallax: true,
     onLeave: function(origin, destination, direction){
-      console.log("Leaving section" + origin.index);
+
       if (origin.index === 1 && direction == 'down') {
         $('.funnel-graph').css({
           'opacity': '0'
         });
         $('.funnel-graph-img').css({
-          // '-webkit-transform': '',
-          // '-ms-transform': '',
           'transform': 'scale(1.2) translate(45%, 1850px)',
           'transition': 'all .9s ease'
         });
+        if (document.documentElement.clientWidth <= 1400) {
+          $('.funnel-graph-img').css({
+            'transform': 'scale(1.2) translate(40%, 1850px)',
+            'transition': 'all .9s ease'
+          });
+        }
+        if (document.documentElement.clientWidth <= 1250) {
+          $('.funnel-graph-img').css({
+            'transform': 'scale(1.2) translate(35%, 1850px)',
+            'transition': 'all .9s ease'
+          });
+        }
+        if (document.documentElement.clientWidth <= 900) {
+          $('.funnel-graph-img').css({
+            'transform': 'scale(1.2) translate(20%, 1850px)',
+            'transition': 'all .9s ease'
+          });
+        }
+        if (document.documentElement.clientWidth <= 1150) {
+          $('.funnel-graph').css({
+            'opacity': '1'
+          });
+        }
       }
       if (origin.index === 2 && direction == 'up') {
         $('.funnel-graph-img').css({
-          // '-webkit-transform': '',
-          // '-ms-transform': '',
           'transform': 'scale(.15) translate(0px, 0px)',
           'transition': 'all .9s ease'
         });
       }
-      if (origin.index === 2 && direction == 'down') {
-        $('.testing').css({
-          'background-position': '110% 0px'
-        })
-        // $('.testing').addClass('onAnimate');
+      
+
+      function stylePosition(sectionNum, vertDirection, selector, posX, posY) {
+        if (origin.index === sectionNum && direction == vertDirection) {
+          $(selector).css({
+            'background-position': `${posX}% ${posY}px`
+          })
+        }
       }
-      if (origin.index === 3 && direction == 'up') {
-        $('.testing').css({
-          'background-position': '110% -1000px'
-        })
-        // $('.testing').addClass('onAnimate');
+      
+      // testing
+      stylePosition(2, 'down', '.testing', '110', '0');
+      stylePosition(3, 'up', '.testing', '110', '-1000');
+
+      if (document.documentElement.clientWidth <= 1400) {
+        stylePosition(2, 'down', '.testing', '150', '0');
+        stylePosition(3, 'up', '.testing', '150', '-1000');
       }
-      if (origin.index === 3 && direction == 'down') {
-        $('.conversion').css({
-          'background-position': '92% 0px'
-        })
+      if (document.documentElement.clientWidth <= 1250) {
+        stylePosition(2, 'down', '.testing', '190', '0');
+        stylePosition(3, 'up', '.testing', '190', '-1000');
       }
-      if (origin.index === 4 && direction == 'up') {
-        $('.conversion').css({
-          'background-position': '92% -1000px'
-        })
+      if (document.documentElement.clientWidth <= 1150) {
+        stylePosition(2, 'down', '.testing', '200', '0');
+        stylePosition(3, 'up', '.testing', '200', '0');
       }
-      if (origin.index === 4 && direction == 'down') {
-        $('.selling').css({
-          'background-position': '65% 0px'
-        })
+      if (document.documentElement.clientWidth <= 768) {
+        stylePosition(2, 'down', '.testing', '0', '0');
+        stylePosition(3, 'up', '.testing', '0', '0');
       }
-      if (origin.index === 5 && direction == 'up') {
-        $('.selling').css({
-          'background-position': '65% -500px'
-        })
+      
+      // conversion
+      stylePosition(3, 'down', '.conversion', '92', '0');
+      stylePosition(4, 'up', '.conversion', '92', '-1000');
+
+      if (document.documentElement.clientWidth <= 1400) {
+        stylePosition(3, 'down', '.conversion', '110', '0');
+        stylePosition(4, 'up', '.conversion', '110', '-1000');
       }
+      if (document.documentElement.clientWidth <= 1250) {
+        stylePosition(3, 'down', '.conversion', '120', '0');
+        stylePosition(4, 'up', '.conversion', '120', '-1000');
+      }
+      if (document.documentElement.clientWidth <= 1150) {
+        stylePosition(3, 'down', '.conversion', '120', '0');
+        stylePosition(4, 'up', '.conversion', '120', '0');
+      }
+      if (document.documentElement.clientWidth <= 1024) {
+        stylePosition(3, 'down', '.conversion', '80', '0');
+        stylePosition(4, 'up', '.conversion', '80', '0');
+      }
+      if (document.documentElement.clientWidth <= 657) {
+        stylePosition(3, 'down', '.conversion', '0', '0');
+        stylePosition(4, 'up', '.conversion', '0', '0');
+      }
+
+      // selling
+      stylePosition(4, 'down', '.selling', '65', '0');
+      stylePosition(5, 'up', '.selling', '65', '-500');
+
+      if (document.documentElement.clientWidth <= 1400) {
+        stylePosition(4, 'down', '.selling', '72', '0');
+        stylePosition(5, 'up', '.selling', '72', '-500');
+      }
+      if (document.documentElement.clientWidth <= 1250) {
+        stylePosition(4, 'down', '.selling', '80', '0');
+        stylePosition(5, 'up', '.selling', '80', '-500');
+      }
+      if (document.documentElement.clientWidth <= 1150) {
+        stylePosition(4, 'down', '.selling', '80', '0');
+        stylePosition(5, 'up', '.selling', '80', '0');
+      }
+      if (document.documentElement.clientWidth <= 900) {
+        stylePosition(4, 'down', '.selling', '70', '0');
+        stylePosition(5, 'up', '.selling', '70', '0');
+      }
+      if (document.documentElement.clientWidth <= 768) {
+        stylePosition(4, 'down', '.selling', '50', '0');
+        stylePosition(5, 'up', '.selling', '50', '0');
+      }
+      if (document.documentElement.clientWidth <= 425) {
+        stylePosition(4, 'down', '.selling', '70', '0');
+        stylePosition(5, 'up', '.selling', '70', '0');
+      }
+
     },
     afterLoad: function(origin, destination, direction) {
-      // console.log("After Load" + direction);
       if (origin.index === 2 && direction == 'up') {
         $('.funnel-graph').css({
           'opacity': '1'
         });
       }
-      if (origin.index === 3) {
-        // $('.testing').css({
-        //   'background-position': '110% 0px'
-        // })
-        // $('.testing').addClass('offAnimate');
+    },
+    afterResize: function(width, height) {
+      if (width < 1400) {
+        // console.log('width');
       }
-      
     },
     scrollingSpeed: 900,
   });
