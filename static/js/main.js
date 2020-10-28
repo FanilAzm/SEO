16fb97af4a63a0e9f75e9d5d4f4a6e2e5f913863
Фаниль Азмагулов <fanilazmagulov@Mac-mini-Fanil.local>
2020-10-28 17:23:12 +0500

    Добавил анимацию наведения на воронку

--- static/js/main.js
+++ static/js/main.js
@@ -18,7 +18,28 @@ $(document).ready(function(){
     mainClass: 'mfp-width-zoom'
   });
 
-  
+  function animateGraph(item, selector) {
+    $(item).hover(function(){
+      $(selector).css({'transform': 'translateX(-10%)'})
+    }, function() {
+      $(selector).css({'transform': 'translateX(0)'})
+    })
+  }
+
+  $('.funnel-item__wrap').each(function(index, item) {
+    if (index === 0) {
+      animateGraph(item, '.funnel-figure__blue');
+    }
+    if (index === 1) {
+      animateGraph(item, '.funnel-figure__green');
+    }
+    if (index === 2) {
+      animateGraph(item, '.funnel-figure__yellow');
+    }
+    if (index === 3) {
+      animateGraph(item, '.funnel-figure__red');
+    }
+  });
 
   $('#fullpage').fullpage({
     navigation: true,
@@ -26,12 +47,13 @@ $(document).ready(function(){
     anchors: ['advertising', 'funnel', 'custom', 'testing', 'conversion', 'selling', 'install', 'price', 'order', 'cases'],
     parallax: true,
     onLeave: function(origin, destination, direction){
-
       if (origin.index === 1 && direction == 'down') {
+
         $('.funnel-graph').css({
           'opacity': '0'
         });
         $('.funnel-graph-img').css({
+          'opacity': '1',
           'transform': 'scale(1.2) translate(45%, 1850px)',
           'transition': 'all .9s ease'
         });
@@ -50,7 +72,7 @@ $(document).ready(function(){
         if (document.documentElement.clientWidth <= 900) {
           $('.funnel-graph-img').css({
             'transform': 'scale(1.2) translate(20%, 1850px)',
-            'transition': 'all .9s ease'
+            'transition': 'all .3s ease'
           });
         }
         if (document.documentElement.clientWidth <= 1150) {
@@ -84,8 +106,8 @@ $(document).ready(function(){
         stylePosition(3, 'up', '.testing', '150', '-1000');
       }
       if (document.documentElement.clientWidth <= 1250) {
-        stylePosition(2, 'down', '.testing', '190', '0');
-        stylePosition(3, 'up', '.testing', '190', '-1000');
+        stylePosition(2, 'down', '.testing', '175', '0');
+        stylePosition(3, 'up', '.testing', '175', '-1000');
       }
       if (document.documentElement.clientWidth <= 1150) {
         stylePosition(2, 'down', '.testing', '200', '0');
@@ -133,6 +155,10 @@ $(document).ready(function(){
         stylePosition(4, 'down', '.selling', '75', '0');
         stylePosition(5, 'up', '.selling', '75', '-500');
       }
+      if (document.documentElement.clientWidth <= 1150) {
+        stylePosition(4, 'down', '.selling', '80', '0');
+        stylePosition(5, 'up', '.selling', '80', '0');
+      }
       if (document.documentElement.clientWidth <= 900) {
         stylePosition(4, 'down', '.selling', '70', '0');
         stylePosition(5, 'up', '.selling', '70', '0');
@@ -151,6 +177,9 @@ $(document).ready(function(){
       if (origin.index === 2 && direction == 'up') {
         $('.funnel-graph').css({
           'opacity': '1'
+        });
+        $('.funnel-graph-img').css({
+          'opacity': '0'
         });
       }
     },
